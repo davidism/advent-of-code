cf2a219ac925da42c936186410796d9bd1c7d028
David Lord <davidism@gmail.com>
2025-12-04 18:03:48 -0800

    2025 01

--- 2025/rust/day01/src/main.rs
+++ 2025/rust/day01/src/main.rs
@@ -1,4 +1,27 @@
+static DATA: &str = include_str!("../../../day01.txt");
+
 fn main() {
-    let data = include_str!("../../../day01.txt");
-    print!("{}", data);
+    let mut dial = 50;
+    let mut code1 = 0;
+    let mut code2 = 0;
+
+    for line in DATA.lines() {
+        let amount: i32 = (if &line[..1] == "L" { -1 } else { 1 }) * &line[1..].parse().unwrap();
+        let end = dial + amount;
+        let mut zeroes = (end / 100).abs();
+
+        if dial != 0 && end <= 0 {
+            zeroes += 1;
+        }
+
+        dial = end.rem_euclid(100);
+
+        if dial == 0 {
+            code1 += 1;
+        }
+
+        code2 += zeroes;
+    }
+
+    println!("{}\n{}", code1, code2);
 }
